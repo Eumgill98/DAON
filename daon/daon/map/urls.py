546119557da835397e1d5f47db7208e657a3119f
wjdghcks6735 <wjdghcks02@gmail.com>
2022-09-30 05:26:39 +0900

    두번째푸시

--- daon/daon/map/urls.py
+++ daon/daon/map/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('chat/', views.chat, name='chat'),
+    path('main/', views.main, name='main'),
+    path('recomand/', views.recomand, name='recomand'),
+    path('make/', views.make, name='make')
+
 ]
--- daon/daon/map/views.py
+++ daon/daon/map/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import folium
 
 from pathlib import Path
 
@@ -8,7 +8,25 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-def index(request):
+def home(request):
 
-    return render(request, 'map/index.html')
 
+    return render(request, 'map/home.html')
+
+def chat(requset):
+
+    return render(requset, 'map/chat.html')
+
+def main(request):
+    map = folium.Map(location=[37.7854, 128.4698], zoom_start=15, width='100%', height='100%', )
+    maps = map._repr_html_()
+
+    return render(request, 'map/main.html', {'map':maps})
+
+def recomand(requset):
+
+    return  render(requset, 'map/recomand.html')
+
+def make(requset):
+
+    return render(requset, 'map/make.html')
