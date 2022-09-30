8c8e5f280292595d60e491181ed66ec765896508
HongTae Kim <y159159@naver.com>
2022-09-30 22:55:30 +0900

    hong1

--- daon/daon/map/views.py
+++ daon/daon/map/views.py
@@ -1,31 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import folium
 
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 def home(request):
-
-
     return render(request, 'map/home.html')
 
 def chat(requset):
-
     return render(requset, 'map/chat.html')
 
 def main(request):
-
-
     return render(request, 'map/main.html')
 
 def recomand(requset):
-
     return  render(requset, 'map/recomand.html')
 
 def make(requset):
-
     return render(requset, 'map/make.html')
