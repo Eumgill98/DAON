ca7e3d65ab8d707e832c709588146a282779f364
wjdghcks6735 <wjdghcks02@gmail.com>
2022-09-30 11:23:47 +0900

    3번째 푸쉬뚜쉬빠쉬

--- daon/daon/map/views.py
+++ daon/daon/map/views.py
@@ -18,10 +18,9 @@ def chat(requset):
     return render(requset, 'map/chat.html')
 
 def main(request):
-    map = folium.Map(location=[37.7854, 128.4698], zoom_start=15, width='100%', height='100%', )
-    maps = map._repr_html_()
 
-    return render(request, 'map/main.html', {'map':maps})
+
+    return render(request, 'map/main.html')
 
 def recomand(requset):
 
